54f50ee51006f8f3bf7b0076ac6536bf0b254013
MS_btff <whdghks845@gmail.com>
2023-06-22 13:18:28 +0900

    react_dr

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -7,7 +7,20 @@ export default function Counter() {
       <span className="number">{count}</span>
       <button
         className="button"
+        // 클로저(스냅샷을 찍는다고 생각하면 된다.) 렉시컬 환경 외부 함수 참조보다 콜백함수 형태로 하는게 좋다
         onClick={() => {
+          // 이건 값이 5씩 증가
+          setCount((prev) => prev + 1);
+          setCount((prev) => prev + 1);
+          setCount((prev) => prev + 1);
+          setCount((prev) => prev + 1);
+          setCount((prev) => prev + 1);
+
+          //이건 값이 하나씩만 증가
+          setCount(count + 1);
+          setCount(count + 1);
+          setCount(count + 1);
+          setCount(count + 1);
           setCount(count + 1);
           console.log(setCount);
         }}
