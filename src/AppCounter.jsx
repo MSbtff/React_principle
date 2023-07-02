6b71f8fee92d40d93ec275f8ed063e5487b1d98f
MS_btff <whdghks845@gmail.com>
2023-07-03 08:40:50 +0900

    react_dr

--- src/AppCounter.jsx
+++ src/AppCounter.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import Counter from './components/Counter';
 import './App.css';
+import {useState} from 'react';
 
 export default function AppCounter() {
+  const [count, setCount] = useState(0);
+  const handleClick = () => setCount((prev) => prev + 1);
   return (
-    <div>
-      <Counter />
+    <div className="container">
+      <div className="banner">
+        Total Count: {count} {count > 10 ? '🔥' : '❄️'}
+      </div>
+
+      <div className="counters">
+        {/* props 는 total 과 onClick 그러니 Counter로 props 전달 해줘야 함 */}
+        <Counter total={count} onClick={handleClick} />
+        <Counter total={count} onClick={handleClick} />
+      </div>
     </div>
   );
 }
+
+// 컴포넌트를 만들어 나갈때 공통적인 데이터는 필요하고 근접한 부모 컴포넌트의 두고
+// 필요한 데이터는 prop으로 전달해주고 클릭했을때 부모에서 처리하고 싶으면 prop으로 전달
+//외부에 전달하는 props 콜백함수 및 외부로 노출하는 API는 우리가 노출하고 싶은 범위 내에서,
+// 최대한 좁혀서 제공해 주는게 좋다.
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,28 +1,33 @@
 import React, {useState} from 'react';
 
-export default function Counter() {
+export default function Counter({total, onClick}) {
   const [count, setCount] = useState(0);
   return (
     <div className="counter">
-      <span className="number">{count}</span>
+      <p className="number">
+        {count}
+        <span className="total">/{total}</span>
+      </p>
       <button
         className="button"
         // 클로저(스냅샷을 찍는다고 생각하면 된다.) 렉시컬 환경 외부 함수 참조보다 콜백함수 형태로 하는게 좋다
         onClick={() => {
-          // 이건 값이 5씩 증가
           setCount((prev) => prev + 1);
-          setCount((prev) => prev + 1);
-          setCount((prev) => prev + 1);
-          setCount((prev) => prev + 1);
-          setCount((prev) => prev + 1);
+          onClick();
+          // // 이건 값이 5씩 증가 비동기적 작용 할 때
+          // setCount((prev) => prev + 1);
+          // setCount((prev) => prev + 1);
+          // setCount((prev) => prev + 1);
+          // setCount((prev) => prev + 1);
+          // setCount((prev) => prev + 1);
 
-          //이건 값이 하나씩만 증가
-          setCount(count + 1);
-          setCount(count + 1);
-          setCount(count + 1);
-          setCount(count + 1);
-          setCount(count + 1);
-          console.log(setCount);
+          //   //이건 값이 하나씩만 증가
+          //   setCount(count + 1);
+          //   setCount(count + 1);
+          //   setCount(count + 1);
+          //   setCount(count + 1);
+          //   setCount(count + 1);
+          //   console.log(setCount);
         }}
       >
         ADD +
